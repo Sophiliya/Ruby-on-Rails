834c6589e86214ed98bcfbe50152d03b0af2050e
Aliya <sophiliya@gmail.com>
2019-06-21 18:57:13 +0600

    Fix by comments

--- date_number.rb
+++ date_number.rb
@@ -3,11 +3,10 @@ day = gets.chomp.to_i
 month = gets.chomp.to_i
 year = gets.chomp.to_i
 
-unless month == 1
-  months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  is_leap_year = ( year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0
-  days = months[0..(month - 2)].inject(&:+) + day
-  day = ((month == 2 && day == 29) || month > 2) && is_leap_year ? days + 1 : days
-end
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+is_leap_year = ( year % 4 == 0 && year % 100 != 0 ) || year % 400 == 0
 
-p "Порядковый номер даты: #{day}"
+days = month > 1 ? months.take(month - 1).sum + day : day
+days += 1 if month > 2 && is_leap_year
+
+p "Порядковый номер даты: #{days}"
