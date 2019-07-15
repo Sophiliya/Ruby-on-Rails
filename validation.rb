b425803534edaa91d342a5fc15668c7b47d8bfbf
Aliya <sophiliya@gmail.com>
2019-07-15 08:46:49 +0600

    Refactor modules

--- validation.rb
+++ validation.rb
@@ -7,6 +7,14 @@ module Validation
 
   module ClassMethods
     def validate(attribute, type, format = nil)
+      create_validation_methods(attribute, type, format)
+      create_validate!(attribute, format)
+      create_valid?
+    end
+
+    private 
+
+    def create_validation_methods(attribute, type, format)
       case type
       when :presence
         define_method(:validate_presence) do |attribute|
@@ -21,7 +29,9 @@ module Validation
           "Атрибут не соответствуют классу #{type}" if attribute.class != type
         end
       end
+    end
 
+    def create_validate!(attribute, format)
       define_method(:validate!) do
         name = instance_variable_get "@#{attribute.to_s.gsub(':', '')}".to_sym
         msg = instance_variable_get :@message
@@ -34,7 +44,9 @@ module Validation
 
         instance_variable_set :@message, msg.compact
       end
+    end
 
+    def create_valid?
       define_method(:valid?) do
         validate!
         msg = instance_variable_get :@message
